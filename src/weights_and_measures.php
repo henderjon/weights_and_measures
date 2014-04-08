7434fe18ab36dfdf1497305a94ebdc44629d6798
henderjon <jhenderson@capstonedigital.com>
2014-04-08 17:34:06 -0500

    Apparently, define is more flexible, but slower. Since these are namespaced, I don't need conditionals or flexibility.
    http://stackoverflow.com/questions/2447791/define-vs-const
    
    Conflicts:
    	src/weights_and_measures.php

--- src/weights_and_measures.php
+++ src/weights_and_measures.php
@@ -5,26 +5,27 @@ namespace WM;
 /**
  * time
  */
-define("ONE_SECOND", 1);
-define("ONE_MINUTE", 60);
-define("ONE_HOUR",   3600);
-define("ONE_DAY",    86400);
-define("ONE_WEEK",   604800);
-define("ONE_MONTH",  2419200);
+const ONE_SECOND = 1;
+const ONE_MINUTE = 60;
+const ONE_HOUR   = 3600;
+const ONE_DAY    = 86400;
+const ONE_WEEK   = 604800;
+const ONE_MONTH  = 2419200;
 
 /**
  * size
  */
-define("ONE_K",  1000);
-define("ONE_KB", 1024);
-define("ONE_M",  1000000);
-define("ONE_MB", 1048576);
-define("ONE_G",  1000000000);
-define("ONE_GB", 1073741824);
+const ONE_K  = 1000;
+const ONE_KB = 1024;
+const ONE_M  = 1000000;
+const ONE_MB = 1048576;
+const ONE_G  = 1000000000;
+const ONE_GB = 1073741824;
 
 /**
  * formats
  */
-define("MYSQL_FORMAT_TIME",     "H:i:s");
-define("MYSQL_FORMAT_DATE",     "Y-m-d");
-define("MYSQL_FORMAT_DATETIME", "Y-m-d H:i:s");
+const MYSQL_FORMAT_TIME     = "H:i:s";
+const MYSQL_FORMAT_DATE     = "Y-m-d";
+const MYSQL_FORMAT_DATETIME = "Y-m-d H:i:s";
+
